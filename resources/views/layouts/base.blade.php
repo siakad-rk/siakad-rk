41e7087b10fa679a5f576b45681fcbc6abdc2c13
Steven Candra <steven.candra5@gmail.com>
2018-05-10 19:17:16 +0700

    adding navbar layout(belum ada login page)
    entar kalo mau akses php artisan serve -> localhost:8000/ udah bisa

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <title>@yield('title')</title>
+    </head>
+    <body>
+        @include('layouts.navbar')
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Sistem Informasi Akademik</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Nilai <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Ulangan Harian</a></li>
+            <li><a href="#">Psikomotorik</a></li>
+            <li><a href="#">Afektif</a></li>
+            <li><a href="#">UTS</a></li>
+            <li><a href="#">UAS</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Ekstrakurikuler</a></li>
+        <li><a href="#">Bantuan</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Data Pribadi</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
